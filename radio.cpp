d0d00ae1d567761f9f672a1e369fa3612b75b1b9
gorillanet <rnsztng@gmail.com>
2017-07-13 00:07:19 +0900

    add signal handler

--- radio.cpp
+++ radio.cpp
@@ -18,20 +18,23 @@ int32_t a ;
 int32_t * ptr;
 int32_t n = 2500;
 int64_t size = sizeof(a)*n;
+static int sig{1};
 //int32_t mm = n-4;
 
-void inline boost_song()
-{
+void sig_handler(int sign) {
+    free(ptr);
+    exit(-1);
+}
+
+void inline boost_song() {
     using namespace std::chrono ;
 
     //int i{0} ;
-    while( true )
-    {
+    while( true ) {
         std::unique_lock<std::mutex> lk{m} ;
         cv.wait( lk ) ;
 
-        while( high_resolution_clock::now() < mid )
-        {
+        while( high_resolution_clock::now() < mid ) {
             int32_t var[4] = { *(ptr), *(ptr + 2), *(ptr + 3), *(ptr + 4) };
             va = vld1q_s32(var);
             //i++;
@@ -41,7 +44,7 @@ void inline boost_song()
     }
 }
 
-int init_memory(void){
+int init_memory(void) {
     ptr = (int32_t *)malloc(size);
     if( ptr == NULL ){
         std::cout << "Malloc Error\n";
@@ -53,8 +56,7 @@ int init_memory(void){
     return 0;
 }
 
-void square_am_signal(float time, float frequency)
-{
+void square_am_signal(float time, float frequency) {
     using namespace std::chrono ;
     
     std::cout << "Playing / " << time << " seconds / " << frequency << " Hz\n" ;
@@ -69,8 +71,7 @@ void square_am_signal(float time, float frequency)
     auto start = high_resolution_clock::now() ;
     auto const end = start + nanoseconds( static_cast<rep>(time * nsec_per_sec) ) ;
 
-    while (high_resolution_clock::now() < end)
-    {
+    while (high_resolution_clock::now() < end) {
         mid = start + period / 2 ;
         reset = start + period ;
         cv.notify_all() ;
@@ -80,14 +81,14 @@ void square_am_signal(float time, float frequency)
 }
 
 int main(){
+    signal(SIGINT, sig_handler);
+
     init_memory();
-    for ( unsigned i = 0 ; i < std::thread::hardware_concurrency() ; ++i )
-    {
+    for ( unsigned i = 0 ; i < std::thread::hardware_concurrency() ; ++i ) {
         std::thread t( boost_song ) ;
         t.detach() ;
     }
-    while (1)
-    {
+    while (1) {
         square_am_signal(0.400, 2673);
         square_am_signal(0.400, 2349);
         square_am_signal(0.400, 2093);
@@ -116,4 +117,5 @@ int main(){
         square_am_signal(0.790, 2093);
     }
     free(ptr);
+    return 0;
 }
