d7d93fdf58967f5999db8e5d211e8f8819c34c48
gorillanet <ymk@mailyk.com>
2017-12-08 10:23:35 +0900

    simd to fp3

--- intel.cpp
+++ intel.cpp
@@ -51,7 +51,7 @@ void square_am_signal(float time, float frequency) {
         reset = start + period ;
         int i = 0;
         while( high_resolution_clock::now() < mid ) {
-            f = 1 - log(f) / 1.7193
+            f = 1 - log(f) / 1.7193;
         }
         std::this_thread::sleep_until( reset ) ;
         start = reset;
